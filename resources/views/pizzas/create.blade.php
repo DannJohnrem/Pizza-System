df5c4b2950613db940792ba460c4b2ae5090c7d1
DannJohnrem <jonrhem10@gmail.com>
2021-04-21 23:19:55 +0800

    minor changes

--- resources/views/pizzas/create.blade.php
+++ resources/views/pizzas/create.blade.php
@@ -2,13 +2,31 @@
 
 
 @section('content')
-<div class="flex-center position-ref full-height">
-    <div class="content">
-        <div class="title m-d-md">
-            Add a new Pizza
-        </div>
-    </div>
+<div class="wrapper create-pizza">
+    <h1>Create a New Pizza</h1>
+
+    <form action="" method="">
+        <label for="name">Enter your name: </label>
+        <input type="text" id="name" name="name">
+
+        <label for="type">Choose pizza type: </label>
+        <select name="type" id="type">
+            <option value="cheese">Cheese Pizza</option>
+            <option value="veggie">Veggie Pizza</option>
+            <option value="pepperoni">Pepperoni Pizza</option>
+            <option value="meat">Meat Pizza</option>
+            <option value="hawaiian">Hawaiian Pizza</option>
+            <option value="supreme">Supreme Pizza</option>
+        </select>
+
+        <label for="base">Choose base type: </label>
+        <select name="base" id="base">
+            <option value="cheese">Thick Crust Pizza</option>
+            <option value="veggie">Thin Crust</option>
+            <option value="pepperoni">Flatbread Crust</option>
+        </select>
+
+        <input type="button" name="submit" value="Order Pizza">
+    </form>
 </div>
 @endsection
-
-
--- resources/views/pizzas/index.blade.php
+++ resources/views/pizzas/index.blade.php
@@ -2,12 +2,16 @@
 
 @section('content')
 
-    <div class="flex-center position-ref full-height">
-        <div class="content">
-            <img src="{{ asset('img/pizza-house1.png') }}" alt="pizzahouse">
-            <div class="title m-b-md">
-                <span>The Pizza Street </p>
-            </div>
+<div class="flex-center position-ref full-height">
+    <div class="content">
+        <img src="{{ asset('img/pizza-house1.png') }}" alt="pizzahouse">
+        <div class="title m-b-md">
+            <span>The Pizza Street </p>
         </div>
+
+        <a href="{{ url('pizzas/create') }}">
+            <button type="button" class="btn btn-outline-primary">Order a Pizza</button>
+        </a>
     </div>
+</div>
 @endsection
